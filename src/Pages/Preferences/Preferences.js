3fc88def648249850ac9f5a9e9d0d93789ab4c85
Jamiu Abdsalami <jamiuabdsalami@outlook.com>
2022-07-29 08:48:49 -0700

    Styles preferences page

--- src/Pages/Preferences/Preferences.js
+++ src/Pages/Preferences/Preferences.js
@@ -1,30 +1,36 @@
 import { v4 as uuid } from 'uuid';
 import menu from '../../data/menu.json';
+import './Prefernces.scss';
+import BackArrow from '../../assets/icons/backarrow.png'
+import {Link } from 'react-router-dom';
 
 const Preferences = ({ handlePreferencesSubmit, history }) => {
 	return (
-		<div>
+		<div className="preferences">
+			<Link className='service__goback'>
+				<img src={BackArrow} alt="back" />
+			</Link>
 			<h1 className="preferences__title">Your Preferences</h1>
-			<form onSubmit={(e) => {
+			<form  className="preferences__form" onSubmit={(e) => {
 				handlePreferencesSubmit(e)
 				history.push('/preferences');
 			}}>
-				<label htmlFor="urgency">How urgent is this visit?
-					<select name="urgency" id="urgency" className="preferences__urgency-select">
-						<option key={uuid()} value="error">Select Urgency</option>
+				<label htmlFor="urgency" className="preferences__form__prompt">How urgent is this visit?
+					<select name="urgency" id="urgency" className="preferences__form__prompt-select">
+						<option key={uuid()} value="error" className="preferences__form__prompt-select-urgency">Select Urgency</option>
 						{menu.urgency.map(type => 
 							<option key={uuid()} value={type}>{type}</option>
 						)}
 					</select>
 				</label>
-				<label htmlFor="gender">Do you have a gender preference for your doctor?
-					<select name="gender" id="gender" className="preferences__gender-select">
+				<label htmlFor="gender" className="preferences__form__prompt">Do you have a gender preference for your doctor?
+					<select name="gender" id="gender" className="preferences__form__prompt-select">
 						{menu.gender.map(type => 
-							<option key={uuid()} value={type}>{type}</option>
+							<option key={uuid()} value={type} className="preferences__form__prompt-select-gender">{type}</option>
 						)}
 					</select>
 				</label>
-				<button className="preferences__button" type="submit">SEARCH</button> 
+				<button className="preferences__form__button" type="submit">SEARCH</button> 
 			</form>
 		</div>	
 	);

97ca3499ea2abaf292aaa86bfbefb53a4fa5b04c
Po Lin <po.ting.lin@gmail.com>
2022-07-29 00:39:35 -0400

    Add structure for state for things we might need

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.scss';
 import MainPage from './Pages/MainPage/MainPage'
 
-function App() {
-  return (
-    <div>
-      <MainPage />
-    </div>
-  );
+class App extends Component{
+	state = {
+		isLoggedIn: false,
+		name: 'Hyunah',
+		zip: null
+	}
+	render(){
+	    return (
+	  		<Router>
+				<Switch>
+			  		<Route path='/' exact component={(routerProps) => {
+						return (
+							<MainPage
+								name={this.state.name}
+								{...routerProps}
+							/>
+						)
+					}}/>
+				</Switch>
+			</Router>
+		);
+	}
 }
 
 export default App;

90993aed7f438c498c82f8c510dfc3bd43505bf3
Jigar Patel <jigar@JP-machine.local>
2022-07-29 11:44:35 -0400

    Fix back logo style

--- src/Pages/ServiceSelect/ServiceSelect.js
+++ src/Pages/ServiceSelect/ServiceSelect.js
@@ -7,7 +7,7 @@ const ServiceSelect = ({ handleServiceSubmit, history }) => {
 
 	return (
 		<div className='service'>
-			<img className="service__back" onClick={() => {history.push('/');}} src={BackArrow} alt="back button" />
+			<img className="service__goback" onClick={() => {history.push('/');}} src={BackArrow} alt="back button" />
 			<h1 className="service__title">Book a Doctor's Visit</h1>
 			<p className="service__prompt">What type of service are you looking for?</p>
 			<form className='service__form' onSubmit={(e) => {
@@ -16,9 +16,9 @@ const ServiceSelect = ({ handleServiceSubmit, history }) => {
 				
 			}}>
 				<select name="services" id="services" className="service__form-select">
-					<option key={uuid()} value="error" className='service__form-select'>Select Service</option>
+					<option key={uuid()} value="error" className='service__form-option'>Select Service</option>
 					{menu.services.map(service => 
-						<option key={uuid()} value={service}>{service}</option>
+						<option key={uuid()} className='service__form-option' value={service}>{service}</option>
 					)}
 				</select>
 				<button className="service__form-btn" type="submit">NEXT</button> 

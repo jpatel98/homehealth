f0d64ced82952ae94efc60fb34f3979dec3ba276
Jamiu Abdsalami <jamiuabdsalami@outlook.com>
2022-07-29 07:45:28 -0700

    Getting feature

--- src/Pages/Preferences/Preferences.js
+++ src/Pages/Preferences/Preferences.js
@@ -4,15 +4,16 @@ import menu from '../../data/menu.json';
 const Preferences = ({ handlePreferencesSubmit, history }) => {
 
 	return (
-		<div>
-			<p className="service__prompt">PREFERENCE SELECT</p>
+		<div className="preferences">
+			<h2 className="preferences__header">Your Preferences</h2>
+			<p className="preferences__prompt">PREFERENCE SELECT</p>
 			<form onSubmit={(e) => {
 				handlePreferencesSubmit(e)
 				history.push('/preferences');
 			}}>
-				<select name="services" id="services" className="service__form">
+				<select name="services" id="services" className="preferences__form">
 				</select>
-				<button className="service__button" type="submit">NEXT</button> 
+				<button className="service__button" type="submit">SEARCH</button> 
 			</form>
 		</div>	
 	);
